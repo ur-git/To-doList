c94772f1169434d4ea4e7b6eeaf019e3d7a1a2ca
Penguin <rawaturvashi68@gmail.com>
2021-01-18 12:14:54 +0530

    Adding template and passing Data

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+let itemArray = ["Buy food", "Make food", "Eat food"];
+
 app.get("/", function (req, res) {
-  console.log("server is running");
+  let dateFormat = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  };
+  let today = new Date();
+  let current = today.toLocaleDateString("en-us", dateFormat);
+
+  res.render("list", { kindOfDay: current, newListItem: itemArray });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newList;
+  itemArray.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
