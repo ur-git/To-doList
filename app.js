65b97118ae71b6c15889d82bb8a5f954dbd7b61d
Penguin <rawaturvashi68@gmail.com>
2021-03-22 16:02:51 +0530

    database setup

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-let itemArray = ["Buy food", "Make food", "Eat food"];
-let workArray = [];
+//database
+//connect database
+mongoose.connect("mongodb://localhost:27017/todolistdb", {
+  useUnifiedTopology: true,
+});
+
+//database schema
+const itemsSchema = {
+  name: String,
+};
+
+//mongoose model
+const Item = mongoose.model("Item", itemsSchema);
+
+//db documents
+const item1 = new Item({
+  name: "welcome to todo list",
+});
+const item2 = new Item({
+  name: "Hit the + button to add new line",
+});
+const item3 = new Item({
+  name: "<--- Hit this to delete item",
+});
+
+const defaultArray = [item1, item2, item3];
+
+//insert items
+Item.insertMany(defaultArray, function (err) {
+  if (err) {
+    console.log("err");
+  } else {
+    console.log("successfully saved to database");
+  }
+});
 
 //root route
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { kindOfDay: day, newListItem: itemArray });
+  res.render("list", { kindOfDay: "Today", newListItem: items });
 });
 
 app.post("/", function (req, res) {
